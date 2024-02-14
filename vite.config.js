31285460e603deb3556c8f776cdf269d5499ad53
Takadenoshi <tasos@kadena.io>
2024-02-14 14:45:44 +0100

    Attempt fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://takadenoshi.github.io/design-system-icons-microsite/",
 })
